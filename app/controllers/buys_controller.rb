506791785741afd16ae8bd0e93e987cb1a4464f2
tsuyoponta <tsuyo412ponta@gmail.com>
2021-03-01 18:53:36 +0900

    Formオブジェクト関連の修正と追加

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,8 +1,35 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_root, only: [:index, :create]
+
   def index
     @buy_address = BuyAddress.new
   end
 
   def create
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_address).permit(:postal_code, :source_area_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    if (current_user.id == @item.user_id) || (current_user.id && @item.buy.present?)
+      redirect_to root_path
+    end
   end
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,15 +1,17 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :source_area_id, :city, :block, :building_name, :phone_number, :buy_id
+  attr_accessor :user_id, :item_id, :postal_code, :source_area_id, :city, :block, :building_name, :phone_number
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: "is invalid. Don't include hyphen(-). Within 11 digits" }
-    validates :source_area_id, :city, :block, :building_name
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
+    validates :phone_number, format: { with: /\A0[5789]0\d{4}\d{4}\z/, message: "Input only number" }
+    validates :city, :block, :building_name
   end
 
+  validates :source_area_id, numericality: { other_than: 1, message: "Select" }
+
   def save
-    Buy.create(user_id: user.id, item_id: item.id)
+    buy = Buy.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, source_area_id: source_area_id, city: city, block: block, building_name: building_name, phone_number: phone_number, buy_id: buy.id)
   end
 end
