fd37bb04d8a10a15a5569af1b46bd979f6659c4f
tsuyoponta <tsuyo412ponta@gmail.com>
2021-03-02 08:05:07 +0900

    クレジット決済機能の実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -10,6 +10,7 @@ class BuysController < ApplicationController
   def create
     @buy_address = BuyAddress.new(buy_params)
     if @buy_address.valid?
+      pay_item
       @buy_address.save
       redirect_to root_path
     else
@@ -20,7 +21,7 @@ class BuysController < ApplicationController
   private
 
   def buy_params
-    params.require(:buy_address).permit(:postal_code, :source_area_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buy_address).permit(:postal_code, :source_area_id, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -31,5 +32,14 @@ class BuysController < ApplicationController
     if (current_user.id == @item.user_id) || (current_user.id && @item.buy.present?)
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buy_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,11 +1,11 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :source_area_id, :city, :block, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :source_area_id, :city, :block, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
     validates :phone_number, format: { with: /\A0[5789]0\d{4}\d{4}\z/, message: "Input only number" }
-    validates :city, :block, :building_name
+    validates :city, :block, :building_name, :token
   end
 
   validates :source_area_id, numericality: { other_than: 1, message: "Select" }
