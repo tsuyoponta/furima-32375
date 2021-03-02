8cb72069289d5746a34dce69ec13875d98825b05
tsuyoponta <tsuyo412ponta@gmail.com>
2021-03-02 16:30:35 +0900

    ログイン状態のユーザーが直接商品編集ページへ遷移しようとすると、トップページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless current_user.id == @item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id && @item.buy.blank?
   end
 end
